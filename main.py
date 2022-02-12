1d88e6ddb6beee0b0dd5387cc2417f3441b4137d
Dhruva S <dhruva.shashi@gmail.com>
2022-02-12 14:26:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from bottle import run, template, get, request
+import os
+import joblib
+
+MLModel = joblib.load('MLModel-regressor.sav')
+
+
+@get('/api')
+def hello():
+    try:
+        param = list(map(float, request.query['param'].split(',')))
+        pred = list(MLModel.predict([param]))
+        return {'ok': True, 'risk': pred[0]}
+    except Exception as e:
+        return {'ok': False, 'error': str(e)}
+
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
+
+
